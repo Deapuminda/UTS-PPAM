078c63dbdf2d65ad036c1bd75b4782805a5992b0
Deapuminda <93767381+Deapuminda@users.noreply.github.com>
2021-11-05 19:46:11 +0700

    Add files via upload

---
+++ UTSPPAM/app/src/main/java/com/example/utsppam/FoodData.java
@@ -0,0 +1,19 @@
+package ccom.example.utsppam;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.example.utsppam.Food;
+
+import java.util.ArrayList;
+
+public class FoodData {
+    public static ArrayList<Food> getData(Context context) {
+        ArrayList<Food> list = new ArrayList<Food>();
+        list.add(new Food("Batagor", "Makananan batagor", 5000, context.getDrawable(R.drawable.batagor)));
+        list.add(new Food("Black Salad", "Makananan Salad", 5000, context.getDrawable(R.drawable.black_salad)));
+        list.add(new Food("Caphuchino", "Makananan Capuhcino", 5000, context.getDrawable(R.drawable.cappuchino)));
+        list.add(new Food("Cheesecake", "Makananan Cheesecake", 5000, context.getDrawable(R.drawable.cheesecake)));
+        return list;
+    }
+}
